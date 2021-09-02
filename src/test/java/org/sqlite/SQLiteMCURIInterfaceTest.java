006db915cbb5fcdecf8da1689920c122f045103d
Guillaume Villena <contact@guillaumevillena.fr>
2021-09-03 00:16:11 +0200

    Migrate SQLiteMC Tests to Junit5

--- src/test/java/org/sqlite/SQLiteMCURIInterfaceTest.java
+++ src/test/java/org/sqlite/SQLiteMCURIInterfaceTest.java
@@ -1,22 +1,23 @@
 package org.sqlite;
 
-import org.junit.Test;
-import org.sqlite.mc.*;
+import org.junit.jupiter.api.Test;
+import org.sqlite.mc.SQLiteMCConfig;
+import org.sqlite.mc.SQLiteMCSqlCipherConfig;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.sql.*;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SQLiteMCURIInterfaceTest {
 
     private static final String SQL_TABLE = "CREATE TABLE IF NOT EXISTS warehouses ("
-        + "	id integer PRIMARY KEY,"
-        + "	name text NOT NULL,"
-        + "	capacity real"
-        + ");";
+            + "	id integer PRIMARY KEY,"
+            + "	name text NOT NULL,"
+            + "	capacity real"
+            + ");";
 
     public String createFile() throws IOException {
         File tmpFile = File.createTempFile("tmp-sqlite", ".db");
@@ -46,14 +47,14 @@ public class SQLiteMCURIInterfaceTest {
         stmt.execute(SQL_TABLE);
     }
 
-    public void plainDatabaseCreate(String dbPath) throws IOException, SQLException {
+    public void plainDatabaseCreate(String dbPath) throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:sqlite:file:" + dbPath);
         applySchema(conn);
         conn.close();
     }
 
     public void cipherDatabaseCreate(String dbPath, String key) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:file:" + dbPath + "?cipher=sqlcipher&legacy=1&kdf_iter=4000&key="+key);
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:file:" + dbPath + "?cipher=sqlcipher&legacy=1&kdf_iter=4000&key=" + key);
         applySchema(connection);
         connection.close();
     }
@@ -70,7 +71,7 @@ public class SQLiteMCURIInterfaceTest {
 
         // 2. Ensure another Connection can read the databse written
         Connection c = plainDatabaseOpen(path);
-        assertTrue("The plain database should be always readable", databaseIsReadable(c));
+        assertTrue(databaseIsReadable(c), "The plain database should be always readable");
         c.close();
 
     }
@@ -78,8 +79,7 @@ public class SQLiteMCURIInterfaceTest {
 
     public Connection cipherDatabaseOpen(String dbPath, String key) throws SQLException {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:file:" + dbPath + "?cipher=sqlcipher&legacy=1&kdf_iter=4000&key="+key);
-            return conn;
+            return DriverManager.getConnection("jdbc:sqlite:file:" + dbPath + "?cipher=sqlcipher&legacy=1&kdf_iter=4000&key=" + key);
         } catch (SQLiteException e) {
             return null;
         }
@@ -96,30 +96,23 @@ public class SQLiteMCURIInterfaceTest {
 
         //2. Ensure db is readable with good Password
         Connection c = cipherDatabaseOpen(path, Key1);
-        assertTrue(
-            String.format("1. Be sure the database with config %s can be read with the key '%s'", config.getClass().getSimpleName(), Key1)
-            , databaseIsReadable(c));
+        assertTrue(databaseIsReadable(c), String.format("1. Be sure the database with config %s can be read with the key '%s'", config.getClass().getSimpleName(), Key1));
         c.close();
 
         //3. Ensure db is not readable without the good password (Using Key2 as password)
         c = cipherDatabaseOpen(path, Key2);
-        assertNull(
-            String.format("2 Be sure the database with config %s cannot be read with the key '%s' (good key is %s)", config.getClass().getSimpleName(), Key2, Key1),
-            c);
+        assertNull(c, String.format("2 Be sure the database with config %s cannot be read with the key '%s' (good key is %s)", config.getClass().getSimpleName(), Key2, Key1));
 
         //4. Rekey the database
         c = cipherDatabaseOpen(path, Key1);
-        assertTrue(String.format("3. Be sure the database with config %s can be read before rekeying with the key '%s' (replacing %s with %s)", config.getClass().getSimpleName(), Key2, Key1, Key2)
-            , databaseIsReadable(c));
+        assertTrue(databaseIsReadable(c), String.format("3. Be sure the database with config %s can be read before rekeying with the key '%s' (replacing %s with %s)", config.getClass().getSimpleName(), Key2, Key1, Key2));
         c.createStatement().execute(String.format("PRAGMA rekey=%s", Key2));
-        assertTrue("4. Be sure the database is still readable after rekeying"
-            , databaseIsReadable(c));
+        assertTrue(databaseIsReadable(c), "4. Be sure the database is still readable after rekeying");
         c.close();
 
         //5. Should now be readable with Key2
         c = cipherDatabaseOpen(path, Key2);
-        assertTrue(String.format("5. Should now be able to open the database with config %s and the new key '%s'", config.getClass().getSimpleName(), Key2)
-            , databaseIsReadable(c));
+        assertTrue(databaseIsReadable(c), String.format("5. Should now be able to open the database with config %s and the new key '%s'", config.getClass().getSimpleName(), Key2));
         c.close();
     }
 
@@ -134,22 +127,18 @@ public class SQLiteMCURIInterfaceTest {
 
         //2. Ensure db is readable with good Password
         Connection c = cipherDatabaseOpen(dbfile, Key1);
-        assertTrue(
-            String.format("1. Be sure the database with config %s can be read with the key '%s'", "SQLCipher", Key1)
-            , databaseIsReadable(c));
+        assertTrue(databaseIsReadable(c), String.format("1. Be sure the database with config %s can be read with the key '%s'", "SQLCipher", Key1));
         c.close();
 
         //3. Ensure not readable with wrong key
         Connection c2 = cipherDatabaseOpen(dbfile, Key2);
-        assertFalse(
-            String.format("2. Be sure the database with config %s cannot be read with the key '%s'", "SQLCipher", Key2)
-            , databaseIsReadable(c2));
+        assertFalse(databaseIsReadable(c2), String.format("2. Be sure the database with config %s cannot be read with the key '%s'", "SQLCipher", Key2));
         c.close();
 
     }
 
     @Test
-    public void sqlCipherDatabaseSpecialKeyTest() throws SQLException, IOException{
+    public void sqlCipherDatabaseSpecialKeyTest() throws SQLException, IOException {
         String dbfile = createFile();
         String Key1 = URLEncoder.encode("Key2&2ax", "utf8");
         String Key2 = "Key2&2ax";
@@ -157,22 +146,16 @@ public class SQLiteMCURIInterfaceTest {
 
         //2. Ensure db is readable with Key1 Password URL access
         Connection c = cipherDatabaseOpen(dbfile, Key1);
-        assertTrue(
-            String.format("1. Be sure the database with config %s can be read with the key '%s'", "SQLCipher", Key1)
-            , databaseIsReadable(c));
+        assertTrue(databaseIsReadable(c), String.format("1. Be sure the database with config %s can be read with the key '%s'", "SQLCipher", Key1));
         c.close();
 
         //3. Make sure we can read the database using the SQL interface
         c = new SQLiteMCSqlCipherConfig().setLegacy(1).setKdfIter(4000).withKey(Key2).createConnection("jdbc:sqlite:file:" + dbfile);
-        assertTrue(
-            String.format("2. Be sure the database is readable using PRAGMA method and key containing special characters")
-            , databaseIsReadable(c));
+        assertTrue(databaseIsReadable(c), "2. Be sure the database is readable using PRAGMA method and key containing special characters");
         c.close();
 
         c = new SQLiteMCSqlCipherConfig().setLegacy(1).setKdfIter(4000).withKey(Key2).useSQLInterface(true).createConnection("jdbc:sqlite:file:" + dbfile);
-        assertTrue(
-            String.format("3. Be sure the database is readable using SQL method and key containing special characters")
-            , databaseIsReadable(c));
+        assertTrue(databaseIsReadable(c), "3. Be sure the database is readable using SQL method and key containing special characters");
         c.close();
     }
 
