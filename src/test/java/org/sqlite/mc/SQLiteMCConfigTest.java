f1d696a862a64510f396143b2af14fc49f4b90d3
Guillaume Villena <contact@guillaumevillena.fr>
2023-09-30 19:47:36 +0200

    Code style update

--- src/test/java/org/sqlite/mc/SQLiteMCConfigTest.java
+++ src/test/java/org/sqlite/mc/SQLiteMCConfigTest.java
@@ -1,12 +1,11 @@
 package org.sqlite.mc;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.Properties;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.sqlite.SQLiteConfig.Pragma.LEGACY_PAGE_SIZE;
+
+import java.util.Properties;
+import org.junit.jupiter.api.Test;
 
 class SQLiteMCConfigTest {
 
@@ -21,8 +20,11 @@ class SQLiteMCConfigTest {
         p = new SQLiteMCConfig.Builder().setLegacyPageSize(1024).build().toProperties();
         assertThat(p.get(LEGACY_PAGE_SIZE.getPragmaName())).isEqualTo("1024");
 
-        assertThatIllegalArgumentException().isThrownBy(() -> new SQLiteMCConfig.Builder().setLegacyPageSize(-1));
-        assertThatIllegalArgumentException().isThrownBy(() -> new SQLiteMCConfig.Builder().setLegacyPageSize(65637));
-        assertThatIllegalArgumentException().isThrownBy(() -> new SQLiteMCConfig.Builder().setLegacyPageSize(1233));
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new SQLiteMCConfig.Builder().setLegacyPageSize(-1));
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new SQLiteMCConfig.Builder().setLegacyPageSize(65637));
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new SQLiteMCConfig.Builder().setLegacyPageSize(1233));
     }
 }
